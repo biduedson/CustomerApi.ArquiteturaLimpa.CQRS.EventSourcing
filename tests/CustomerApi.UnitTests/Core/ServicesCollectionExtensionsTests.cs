bfa7cbc552c7bb6c1f9e5e1d6facf82406782c27
Edson Gomes <biduzao.bidu21@gmail.com>
2026-05-08 15:47:52 -0300

    feat: add core extensions tests

--- tests/CustomerApi.UnitTests/Core/ServicesCollectionExtensionsTests.cs
+++ tests/CustomerApi.UnitTests/Core/ServicesCollectionExtensionsTests.cs
@@ -1,24 +1,24 @@
-﻿
+﻿using CustomerApi.Core;
 using CustomerApi.Core.AppSettings;
 using CustomerApi.Core.Extensions;
 using FluentAssertions;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 using Xunit.Categories;
 
 namespace CustomerApi.UnitTests.Core;
 
 [UnitTest]
-public class ConfigurationExtensionsTests
+public class ServicesCollectionExtensionsTests
 {
     [Fact]
-    public void Should_ReturnsClassOptions_WhenGetOptions()
+    public void Should_ReturnClassOptions_WhenConfigureAppSettings()
     {
         const int absoluteExpirationInHours = 4;
         const int slidingExpirationInSeconds = 120;
 
         var configurationBuilder = new ConfigurationBuilder();
-
         configurationBuilder.AddInMemoryCollection(new Dictionary<string, string?>
         {
             { "CacheOptions:AbsoluteExpirationInHours", absoluteExpirationInHours.ToString() },
@@ -27,10 +27,17 @@ public class ConfigurationExtensionsTests
 
         var configuration = configurationBuilder.Build();
 
-        var act = configuration.GetOptions<CacheOptions>();
+        var services = new ServiceCollection();
+        services.AddSingleton<IConfiguration>(_ => configuration);
+        services.ConfigureAppSettings();
+        var serviceProvider = services.BuildServiceProvider(true);
+
+        var act = serviceProvider.GetOptions<CacheOptions>();
 
         act.Should().NotBeNull();
         act.AbsoluteExpirationInHours.Should().Be(absoluteExpirationInHours);
         act.SlidingExpirationInSeconds.Should().Be(slidingExpirationInSeconds);
     }
+
+
 }

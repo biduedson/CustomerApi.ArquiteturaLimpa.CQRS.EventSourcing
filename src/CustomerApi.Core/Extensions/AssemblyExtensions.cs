c97a4e0dbfcc2af79794cd1970b855a9c5f45704
Edson Gomes <biduzao.bidu21@gmail.com>
2026-05-07 22:48:29 -0300

    feat: core extensions setup

---
+++ src/CustomerApi.Core/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CustomerApi.Core.Extensions;
+
+public static class AssemblyExtensions
+{
+    public static IEnumerable<Type> GetAllTypesOf<TInterface>(this Assembly assembly)
+    {
+        var isAssignableToInterface = typeof(TInterface).IsAssignableFrom;
+        return [..assembly
+            .GetTypes()
+            .Where(type => type.IsClass && !type.IsAbstract && !type.IsInterface && isAssignableToInterface(type))
+            ];
+    }
+}

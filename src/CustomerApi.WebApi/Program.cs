8be25d6ab0ce8ec5f5053b98f17b417cbb1ed427
Edson Gomes <biduzao.bidu21@gmail.com>
2026-05-11 13:44:29 -0300

    feat: add configue compression

--- src/CustomerApi.WebApi/Program.cs
+++ src/CustomerApi.WebApi/Program.cs
@@ -1,34 +1,34 @@
 
-namespace CustomerApi.WebApi
+
+using System.IO.Compression;
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+   .Configure<GzipCompressionProviderOptions>(compressionOptions => compressionOptions.Level = CompressionLevel.Fastest);
+
+
+        // Add services to the container.
+ builder.Services.AddControllers();
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+ builder.Services.AddOpenApi();
+
+ var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+ if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+  app.MapOpenApi();
 }
+
+ app.UseHttpsRedirection();
+
+ app.UseAuthorization();
+
+
+ app.MapControllers();
+
+ app.Run();
+    
+

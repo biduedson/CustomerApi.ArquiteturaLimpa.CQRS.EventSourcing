efc9e90d29a594ddfaec22ff900f7bde0fa76f9e
Edson Gomes <biduzao.bidu21@gmail.com>
2026-05-12 13:12:12 -0300

    feat: add customer write repository

---
+++ src/CustomerApi.Domain/Entities/CustomerAggregate/ICustomerWriteOnlyRepository.cs
@@ -0,0 +1,11 @@
+﻿using CustomerApi.Core.SharedKernel;
+using CustomerApi.Domain.ValueObjects;
+
+namespace CustomerApi.Domain.Entities.CustomerAggregate;
+
+public interface ICustomerWriteOnlyRepository : IWriteOnlyRepository<Customer,Guid>
+{
+    Task<bool> ExistsByEmailAsyn(Email email);
+
+    Task<bool> ExistsByEmailAsyn(Email email, Guid currentId);
+}

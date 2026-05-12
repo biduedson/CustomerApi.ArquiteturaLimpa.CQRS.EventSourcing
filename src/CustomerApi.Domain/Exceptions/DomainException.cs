35845cbcc3006994462f20762a8e2b7832a80f61
Edson Gomes <biduzao.bidu21@gmail.com>
2026-05-12 13:06:02 -0300

    feat: add domain exceptions

---
+++ src/CustomerApi.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomerApi.Domain.Exceptions;
+
+public class DomainException : Exception
+{
+    public DomainException(string message)
+        : base(message) { }
+
+    public DomainException(string message, Exception innerException)
+    : base(message, innerException) { }
+
+    public static void ThrowIf(bool condition, string message)
+    {
+        if(condition) throw new DomainException(message);
+    }
+    public static void ThrowIfNull(object? obj, string message)
+    { 
+        if(obj is  null) throw new DomainException(message);
+    }
+
+}

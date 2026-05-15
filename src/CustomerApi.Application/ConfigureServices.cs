02347f35cb21592ba6fb7f4abb6e7fd722bf4c98
Edson Gomes <biduzao.bidu21@gmail.com>
2026-05-15 15:00:57 -0300

    feat: add  configuration of services

--- src/CustomerApi.Application/ConfigureServices.cs
+++ src/CustomerApi.Application/ConfigureServices.cs
@@ -1,5 +1,21 @@
-﻿namespace CustomerApi.Application;
+﻿using System.Reflection;
+using CustomerApi.Application.Abstractions;
+using CustomerApi.Application.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CustomerApi.Application;
 
 public static class ConfigureServices
 {
+    public static IServiceCollection AddCommandHandlers(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetAssembly(typeof(IApplicationMarker));
+
+        return services
+            .AddValidatorsFromAssembly(assembly, ServiceLifetime.Singleton)
+            .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly!)
+            .AddBehavior(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>)));
+    }
 }

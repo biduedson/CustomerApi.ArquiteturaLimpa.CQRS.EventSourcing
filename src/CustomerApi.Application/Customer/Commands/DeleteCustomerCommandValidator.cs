121ba87898f615cdd450dfb3b0fbdb03c8c7cca6
Edson Gomes <biduzao.bidu21@gmail.com>
2026-05-15 14:03:23 -0300

    feat: add delete customer  commands  and hadlers

---
+++ src/CustomerApi.Application/Customer/Commands/DeleteCustomerCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Result;
+using FluentValidation;
+
+namespace CustomerApi.Application.Customer.Commands;
+
+public class DeleteCustomerCommandValidator : AbstractValidator<DeleteCustomerCommand>
+{
+    public DeleteCustomerCommandValidator()
+    {
+        RuleFor(command => command.Id)
+             .NotEmpty();
+    }
+}

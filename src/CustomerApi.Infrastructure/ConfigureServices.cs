84ce185cb6b6f302a03d8ccef43b40718c785001
Edson Gomes <biduzao.bidu21@gmail.com>
2026-05-19 17:41:53 -0300

    feat: add cache services

--- src/CustomerApi.Infrastructure/ConfigureServices.cs
+++ src/CustomerApi.Infrastructure/ConfigureServices.cs
@@ -1,14 +1,21 @@
-﻿using CustomerApi.Core.SharedKernel;
+using CustomerApi.Core.SharedKernel;
 using CustomerApi.Domain.Entities.CustomerAggregate;
 using CustomerApi.Infrastructure.Data;
 using CustomerApi.Infrastructure.Data.Context;
 using CustomerApi.Infrastructure.Data.Repositories;
+using CustomerApi.Infrastructure.Data.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CustomerApi.Infrastructure;
 
 public static class ConfigureServices
 {
+
+    public static void AddMemoryCacheService(this IServiceCollection services) =>
+        services.AddScoped<ICacheService, MemoryCacheService>();
+
+    public static void AddDistributedCacheService(this IServiceCollection services) =>
+        services.AddScoped<ICacheService, DistributedCacheService>();
     public static IServiceCollection AddInfrastructure(this IServiceCollection services) =>
       services
           .AddScoped<WriteDbContext>()

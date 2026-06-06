0076946b5659a9d03dcdbe7acbdac8df5ec3e81e
Edson Gomes <biduzao.bidu21@gmail.com>
2026-06-06 13:17:39 -0300

    feat(webapi): add UserSession migration

--- src/CustomerApi.WebApi/Migrations/UserSession/20260604193231_AddUserSessionAggregate.Designer.cs
+++ src/CustomerApi.WebApi/Migrations/UserSession/20260604193231_AddUserSessionAggregate.Designer.cs
@@ -4,16 +4,19 @@ using CustomerApi.Infrastructure.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace CustomerApi.WebApi.Migrations
+namespace CustomerApi.WebApi.Migrations.UserSession
 {
     [DbContext(typeof(WriteDbContext))]
-    partial class WriteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260604193231_AddUserSessionAggregate")]
+    partial class AddUserSessionAggregate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

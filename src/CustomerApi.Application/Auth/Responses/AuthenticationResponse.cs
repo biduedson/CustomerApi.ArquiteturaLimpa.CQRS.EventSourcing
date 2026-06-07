1399fa89a0e8c5c0659fd66496048962e8a49958
Edson Gomes <biduzao.bidu21@gmail.com>
2026-06-06 21:40:44 -0300

    feat(application): add login command handler

--- src/CustomerApi.Application/Auth/Responses/AuthenticationResponse.cs
+++ src/CustomerApi.Application/Auth/Responses/AuthenticationResponse.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace CustomerApi.Application.Authentication.Responses;
+namespace CustomerApi.Application.Auth.Responses;
 
 public sealed record AuthenticationResponse(
     string AccessToken,

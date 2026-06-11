7cac6cb3261dc5f62fd2633c9ab7794e490e3c12
Edson Gomes <biduzao.bidu21@gmail.com>
2026-06-10 21:22:55 -0300

    fix(domain): support session revocation

--- src/CustomerApi.Domain/Entities/UserSessionAggregate/IUserSessionWriteOnlyRepository.cs
+++ src/CustomerApi.Domain/Entities/UserSessionAggregate/IUserSessionWriteOnlyRepository.cs
@@ -5,6 +5,8 @@ namespace CustomerApi.Domain.Entities.UserSessionAggregate;
 public interface IUserSessionWriteOnlyRepository : IWriteOnlyRepository<UserSession, Guid>
 {
     Task<UserSession?> GetByRefreshTokenHashAsync(string refreshTokenHash);
+    Task<UserSession?> GetByUserAgentAsync(string userAgent);
     Task<List<UserSession>> GetActiveByUserIdAsync(Guid userId);
     Task RevokeAllByUserIdAsync(Guid userId, string reason);
+
 }
